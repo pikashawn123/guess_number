affed7215a2d4821b251a5f0d5785ef000dd2b65
Shawn chiu <shawn20111207@gmail.com>
2024-08-05 23:02:43 +0800

    add the way that code will stop you from doing weird things

--- guess.py
+++ guess.py
@@ -4,15 +4,23 @@ minimum = int(minimum)
 maximum = input('請輸入隨機數的最大值: ')
 maximum = int(maximum)
 answer = random.randint(minimum, maximum)
+guess_max = maximum
+guess_min = minimum
 guess_times = 0
 while True:
-    guess_times = guess_times + 1
     guess = input('請輸入要猜的數字: ')
     guess = int(guess)
+    if guess < guess_max and guess > guess_min:
+        guess_times = guess_times + 1
+    else:
+        print('請不要亂輸入，不然就扣分喔!!!')
+        continue
     if guess > answer:
         print('太大了，請輸入小一點')
+        guess_max = guess
     elif guess < answer:
-        print('太小了，輸入大一點')
+        print('太小了，請輸入大一點')
+        guess_min = guess
     else:
         print('你終於猜對了!!!')
         break
